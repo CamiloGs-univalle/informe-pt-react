bf056f71fb28f61dfd3919c9aa5b6bb99eb873b7
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2026-08-25 00:05:10 -0500

    fix: update Google OAuth Client ID to new credentials

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@
 
 // Tu Client ID de Google Cloud Console
 // Lo encuentras en: console.cloud.google.com > APIs & Services > Credentials
-export const GOOGLE_CLIENT_ID = '1083881497523-90eh2jng0l34u76dv2981kpuk2k99v0k.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_ID = '1083881497523-uvedkll40acpcjoecq5l2fusq3bm5ufh.apps.googleusercontent.com';
 
 // Scopes que necesitamos (solo lectura de Drive)
 export const GOOGLE_SCOPES = [
